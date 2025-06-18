dbf6b502d81d7da48f0acd2b7183565cf37f9bc1
MathisJean <mathisojean@hotmail.com>
2025-06-18 17:41:02 -0300

    Changing Port on Server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 
 let host = "0.0.0.0";
-let port = process.env.PORT || 3000;
+let port = 8080;
 
 setInterval(() => {
   console.log("Keep-alive ping", new Date().toISOString());
